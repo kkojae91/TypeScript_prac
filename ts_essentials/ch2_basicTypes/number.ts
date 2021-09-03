f22498eb8d40b7545b2700ead952e21a591e13aa
kkojae <jaejeung3210@gmail.com>
2021-09-03 19:47:17 +0900

    ts number type

---
+++ ts_essentials/ch2_basicTypes/number.ts
@@ -0,0 +1,15 @@
+// 사용하는 방법은 아래와 같이!
+// 10진수, 16진수, 2진수, 8진수 모두 사용할 수 있다.
+// NaN, underScore도 지원한다.
+let decimal: number = 6;
+
+let hex: number = 0xf00d;
+
+let binary: number = 0b1010;
+
+let octal: number = 0o744;
+
+let notANumber: number = NaN;
+
+// 백만을 아래와 같이 표기할 수 있다.
+let underscoreNum: number = 1_000_000;
