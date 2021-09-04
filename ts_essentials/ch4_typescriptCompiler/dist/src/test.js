86223737d380ed7765c56c61f3e0a6545eb76a42
kkojae <jaejeung3210@gmail.com>
2021-09-04 16:17:31 +0900

    ts compileOptions - outDir, outFile, rootDir options

--- ts_essentials/ch4_typescriptCompiler/dist/src/test.js
+++ ts_essentials/ch4_typescriptCompiler/dist/src/test.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // const a: number = undefined;
-const hello = () => { };
+const hello = () => {
+    console.log("hello");
+};
+console.log("hello");
--- ts_essentials/ch4_typescriptCompiler/src/test.ts
+++ ts_essentials/ch4_typescriptCompiler/src/test.ts
@@ -5,3 +5,5 @@ import React from "react";
 const hello = () => {
   console.log("hello");
 };
+
+console.log("hello");
